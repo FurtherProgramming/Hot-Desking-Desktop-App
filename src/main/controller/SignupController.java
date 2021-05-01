2fffc0b2aff1d3a32555145e816676cdfdf1ac03
LedyLedy <s3409664@student.rmit.edu.au>
2021-05-01 15:17:57 +1000

    third commit

---
+++ src/main/controller/SignupController.java
@@ -0,0 +1,21 @@
+package main.controller;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import main.Admin.AdminController;
+import main.Employee.EmployeeController;
+import main.option;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+public class SignupController {
+}
