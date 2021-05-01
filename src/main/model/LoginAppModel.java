6511721db8b4155512bbd20d7f0fb486711fcf0c
LedyLedy <s3409664@student.rmit.edu.au>
2021-05-01 17:44:02 +1000

    new LoginAppModel.java instead of LoginModel.java

---
+++ src/main/model/LoginAppModel.java
@@ -0,0 +1,121 @@
+package main.model;
+
+import main.SQLConnection;
+import org.sqlite.SQLiteConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class LoginAppModel {
+    Connection connection;
+
+    public LoginAppModel(){
+
+        connection = SQLConnection.connect();
+        if (connection == null)
+            System.exit(1);
+
+    }
+
+    public Boolean isDbConnected(){
+        try {
+            return !connection.isClosed();
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    public Boolean isLogin(String user, String pass, String option) throws Exception {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet=null;
+        String query = "select * from employee where username = ? and password = ? and role = ?";
+        try {
+
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, user);
+            preparedStatement.setString(2, pass);
+            preparedStatement.setString(3, option);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            return false;
+        }finally {
+            preparedStatement.close();
+            resultSet.close();
+        }
+
+    }
+
+    public Boolean isAdminLogin(String user, String pass) throws Exception {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet=null;
+        String query = "select * from employee where role = 'admin' and username = ? and password = ?";
+        try {
+
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, user);
+            preparedStatement.setString(2, pass);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            return false;
+        }finally {
+            preparedStatement.close();
+            resultSet.close();
+        }
+
+    }
+
+
+    public Boolean isEmployeeLogin(String user, String pass) throws Exception {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet=null;
+        String query = "select * from employee where role = 'employee' and username = ? and password = ?";
+        try {
+
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, user);
+            preparedStatement.setString(2, pass);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            return false;
+        }finally {
+            preparedStatement.close();
+            resultSet.close();
+        }
+
+    }
+
+}
